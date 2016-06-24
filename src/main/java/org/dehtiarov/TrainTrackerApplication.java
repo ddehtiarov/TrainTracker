6e5c1cd48f28ee3a4c05588544256a318c2a4cee
Dmytro Dehtiarov <dmytro_dehtiarov@epam.com>
2016-06-24 15:53:16 +0300

    init commit

---
+++ src/main/java/org/dehtiarov/TrainTrackerApplication.java
@@ -0,0 +1,12 @@
+package org.dehtiarov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTrackerApplication.class, args);
+	}
+}
